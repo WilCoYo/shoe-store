6adca652b24703db40fb824dde30c2b174db911a
W. Cody Yonkin <WilCoYonkin@gmail.com>
2025-02-11 16:24:19 -0500

    first works on mobile responsiveness

--- src/assets/components/header/Header.js
+++ src/assets/components/header/Header.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 // import search_icon from '../../assets/images/search-icon.png'
 import heart_icon from '../../images/heart-icon.png'
 import cart_icon from '../../images/cart-icon.png'
+import menu_icon from '../../images/menu-icon.png'
+import close_icon from '../../images/close-icon.png'
+
 
 function Header() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const mobileNavOpen = () => {
+    setMenuOpen(true);
+  }
+
+  const mobileNavClose = () => {
+    if(menuOpen === 'false') {
+      setMenuOpen(true);
+    }
+    else {
+      setMenuOpen(false);
+    }
+    
+  }
+
   return (
     <div className='full-header-component'>
        
-            <div className='left-header'>
+            <div className={!menuOpen ? 'left-header' : 'mobile'}>
                 <h1 id='brand-name'>Shoest.</h1>
             </div>
             <div className='right-header'>
@@ -19,6 +39,30 @@ function Header() {
                     <button>Login</button>
                     <button>Menu</button>
                 </ul>
+            </div>
+            <div className='mobile-header mobile'>
+              <img 
+                src={menu_icon} 
+                alt='hamburger google icon'
+                className={!menuOpen ? 'menu-icon' : 'mobile'}
+                onClick={mobileNavOpen}/>
+                
+            </div>
+            <div className={menuOpen ? 'mobile-navigation' : 'mobile'}>
+              <div className='loginLogout'>
+                <button>Login</button>
+                <img 
+                  src={close_icon} 
+                  alt="'X' close button"
+                  onClick={mobileNavClose}/>
+              </div>
+              
+              <input type='text' placeholder='search'/>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Shop</li>
+              </ul>
             </div>
         
     </div>
